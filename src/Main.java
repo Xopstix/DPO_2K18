86c694bfd1aadabbdb32e5fda75daf03b79c7999
xaviamorcastillo <xavi.amor@gmail.com>
2018-03-14 17:49:54 +0100

    Vista Principal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import Vistas.VistaPrincipal;
+
+import javax.swing.*;
+
 /**
  * Created by xaviamorcastillo on 12/3/18.
  */
 public class Main {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                VistaPrincipal vista = new VistaPrincipal();
+
+                vista.setVisible(true);
+            }
+        });
+
+    }
+
 }
