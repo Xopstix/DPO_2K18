b5495ea7f0599f0b1366604786d75fa16b4d2493
manuelsahun2015 <manuel.sahun.2015@salleurl.edu>
2018-03-15 08:57:25 +0100

    prueba

--- src/Vistas/VistaPrincipal.java
+++ src/Vistas/VistaPrincipal.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 /**
- * Created by xaviamorcastillo on 12/3/18.
+ * Created by xavipargela on 12/3/18.
  */
 public class VistaPrincipal extends JFrame {
 
